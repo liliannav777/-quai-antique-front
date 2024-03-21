486967489e1c83a7aeb1b160c1fa36934ea9e24e
liliannav777 <162148949+liliannav777@users.noreply.github.com>
2024-03-21 21:18:00 +0100

    gestion de la connexion/ deconnexion

---
+++ js/auth/signin.js
@@ -0,0 +1,22 @@
+const mailInput = document.getElementById("EmailInput");
+const passwordInput = document.getElementById("PasswordInput");
+const btnSignin = document.getElementById("btnSignin");
+
+btnSignin.addEventListener("click", checkCredentials);
+
+function checkCredentials(){
+    // Ici il faudra appeler l'Api pour vérifier les credentials en BDD
+
+if(mailInput.value == "test@mail.com" && passwordInput.value == "123"){
+     //Il faudra récupérer le vrai token
+    const token ="erhrsyhyrt";
+    setToken(token);
+    // placer ce token en cookie
+    setCookie(RoleCookieName, "admin", 7)
+    window.location.replace("/");
+}
+else{
+    mailInput.classList.add("is-invalid");
+    passwordInput.classList.add("is-invalid");
+}
+}
